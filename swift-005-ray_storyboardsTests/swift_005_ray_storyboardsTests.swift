c5eb46815e523c6461b466677b29f63d8171220b
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-12 15:01:07 -0300

    Initial Commit

---
+++ swift-005-ray_storyboardsTests/swift_005_ray_storyboardsTests.swift
@@ -0,0 +1,33 @@
+//
+//  swift_005_ray_storyboardsTests.swift
+//  swift-005-ray_storyboardsTests
+//
+//  Created by Luiz Carlos da Silva Araujo on 12/08/21.
+//
+
+import XCTest
+@testable import swift_005_ray_storyboards
+
+class swift_005_ray_storyboardsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
